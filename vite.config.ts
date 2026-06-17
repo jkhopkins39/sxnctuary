af73961d379fb22c0695999213aec3952baba25c
jkhopkins39 <jeremyyhop@gmail.com>
2026-06-17 15:46:42 -0400

    Fix for supabase

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env': process.env
+    'import.meta.env.SUPABASE_URL': JSON.stringify(process.env.SUPABASE_URL),
+    'import.meta.env.SUPABASE_PUBLISHABLE_KEY': JSON.stringify(process.env.SUPABASE_PUBLISHABLE_KEY),
   },
   server: {
     port: 5173
